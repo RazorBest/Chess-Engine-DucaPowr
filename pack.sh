085ef2b257b2280c61db8eb0237f0ae8124892a7
Marius Razvan Pricop <razvan.pricop@protonmail.com>
2021-03-31 08:16:11 +0100

    Check zip file size to be smaller than 5MB

--- pack.sh
+++ pack.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 rm Etapa1.zip
 cd src
 make clean
 zip ../Etapa1.zip *
+
+
+if [ $(du --block-size=1M ../Etapa1.zip | awk '{print $1}') -ge 5 ]
+then
+    echo "ERROR: zip file might be bigger than 5MB"
+    exit 1
+fi
