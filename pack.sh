f6e223eb74153e082742c7eae4ea50bcf2a0cf9d
Marius Razvan Pricop <razvan.pricop@protonmail.com>
2021-04-22 17:57:26 +0100

    Change zip name

--- pack.sh
+++ pack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm Etapa1.zip
+rm Etapa2.zip
 cd src
 make clean
-zip ../Etapa1.zip *
+zip ../Etapa2.zip *
 
 
-if [ $(du --block-size=1M ../Etapa1.zip | awk '{print $1}') -ge 5 ]
+if [ $(du --block-size=1M ../Etapa2.zip | awk '{print $1}') -ge 5 ]
 then
     echo "ERROR: zip file might be bigger than 5MB"
     exit 1
